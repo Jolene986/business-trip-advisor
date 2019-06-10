00272a9942959b00546901fa43a0bf806cc18142
andrijaDomazet <domazet.andrija@gmail.com>
2019-06-10 13:15:58 +0200

    Dodato dugme na home page

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import "./Home.scss";
+import Button from '../../UI/Button/Button'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <p>Home - login page</p>
+                <Button  class = {'button'} > Dugme</Button>
             </div>
         )
     }
